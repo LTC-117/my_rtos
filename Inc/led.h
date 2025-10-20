e4c8ad78bedefe0e962129c3be15d1a09ca79879
LTC-117 <lucastentoni@gmail.com>
2025-10-20 16:01:08 -0300

    [Implemented simple functions for initializing and manipulating Embedded LED states]

--- Inc/led.h
+++ Inc/led.h
@@ -2,4 +2,11 @@
 #define __LED_H__
 
 
+
+void led_init(void);
+void led_on(void);
+void led_off(void);
+void led_toggle(void);
+
+
 #endif
--- Src/led.c
+++ Src/led.c
@@ -1,3 +1,38 @@
 #include "led.h"
 
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_gpio.h"
+#include "stm32f103xb.h"
 
+
+void led_init(void)
+{
+    GPIO_InitTypeDef embedded_led = {
+        .Pin = GPIO_PIN_13,
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FREQ_MEDIUM,
+    };
+
+    HAL_GPIO_Init(GPIOC, &embedded_led);
+}
+
+
+void led_on(void)
+{
+    // 'reset' because 'set' sets the pin 13, which resets the led itself,
+    // and vice-versa
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+}
+
+
+void led_off(void)
+{
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+}
+
+
+void led_toggle(void)
+{
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+}
