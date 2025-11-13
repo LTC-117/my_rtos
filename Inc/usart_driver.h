f5db730f59511c6a1494c090abb9450d8437cc61
LTC-117 <lucastentoni@gmail.com>
2025-11-13 00:23:14 -0300

    [Exporting the 'usart_write()' function]

--- Inc/usart_driver.h
+++ Inc/usart_driver.h
@@ -6,7 +6,8 @@
 #include "stm32f1xx_hal_usart.h"
 
 
-void usart_tx_rx_init(USART_TypeDef *usart_module, uint32_t baud_rate);
+USART_HandleTypeDef usart_tx_rx_init(USART_TypeDef *usart_module, uint32_t baud_rate);
+void usart_write(USART_HandleTypeDef *huart, char *message);
 
 
 #endif
